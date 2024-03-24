e3d844f3ef8acd603b1c2d3885b68a0bbe1c890c
Ghazanfar Ali <gak_92@hotmail.com>
2024-03-24 14:23:50 +0500

    add button and generate travel plan if fields are filled

--- app.py
+++ app.py
@@ -26,17 +26,21 @@ duration = st.sidebar.slider('Duration', 1, 10, 5)
 def get_the_planner(message):
   # get model
   model = genai.GenerativeModel('gemini-pro')
-  chat = model.start_chat()
-
-  response = chat.send_message(message)
+  response = model.generate_content(message)
   return response.text
 
 # create a message to send to the model using the user input
 message = f"Plan a trip from {source} to {destination} on {date} with a budget of ${budget} for {duration} days."
 
-# get the response from the model
-response = get_the_planner(message)
 
-# display the response
-st.write(response)
-
+# display the response when clicked on button
+if st.sidebar.button('Generate Travel Plan'):
+    # generate travel plan if all fields are filled else display error message
+    if source and destination and date and budget and duration:
+        # show message till get response 
+        with st.spinner('Generating Travel Plan...'):
+            response = get_the_planner(message)
+        st.success('Here is your generated travel plan:')
+        st.markdown(response)
+    else:
+        st.error('Please fill in all the fields to generate the travel plan.')
