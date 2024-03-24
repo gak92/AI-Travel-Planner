31f6afc7d07f1b640fe335e01ba337a0ae2429ab
Ghazanfar Ali <gak_92@hotmail.com>
2024-03-24 14:37:50 +0500

    get api key as input from the user

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import datetime
 
 # configure key
 # use the environment variable GOOGLE_API_KEY for api access
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+# genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 # Title
 st.title('AI Travel Planner')
@@ -15,16 +15,21 @@ st.title('AI Travel Planner')
 st.subheader('Plan your next trip with AI')
 
 # Sidebar
-st.sidebar.header('User Input Parameters')
+st.sidebar.header('Enter details to generate a travel plan:')
+
+# get google api key from the user but don't display it
+api_key = st.sidebar.text_input('Enter Google API Key')
+
 # source, destination, date, budget, duration
 source = st.sidebar.text_input('Source', 'New York')
 destination = st.sidebar.text_input('Destination', 'Los Angeles')
-date = st.sidebar.date_input('Travel Start Date', datetime.date(2021, 1, 1)).strftime('%Y-%m-%d')
+date = st.sidebar.date_input('Travel Start Date', "today").strftime('%Y-%m-%d')
 budget = st.sidebar.number_input('Budget', 1000)
-duration = st.sidebar.slider('Duration', 1, 10, 5)
+duration = st.sidebar.slider('Duration', 1, 90, 5)
 
-def get_the_planner(message):
+def get_the_planner(message, api_key):
   # get model
+  genai.configure(api_key=api_key)
   model = genai.GenerativeModel('gemini-pro')
   response = model.generate_content(message)
   return response.text
@@ -36,10 +41,10 @@ message = f"Plan a trip from {source} to {destination} on {date} with a budget o
 # display the response when clicked on button
 if st.sidebar.button('Generate Travel Plan'):
     # generate travel plan if all fields are filled else display error message
-    if source and destination and date and budget and duration:
+    if api_key and source and destination and date and budget and duration:
         # show message till get response 
         with st.spinner('Generating Travel Plan...'):
-            response = get_the_planner(message)
+            response = get_the_planner(message, api_key)
         st.success('Here is your generated travel plan:')
         st.markdown(response)
     else:
